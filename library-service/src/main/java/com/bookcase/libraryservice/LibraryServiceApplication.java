6425f21fb1caad9392d3c705c7fa8c9341837268
talharic@gmail.com <talharic@gmail.com>
2023-11-25 04:11:24 +0300

    create error decoder and feign logger bean

--- library-service/src/main/java/com/bookcase/libraryservice/LibraryServiceApplication.java
+++ library-service/src/main/java/com/bookcase/libraryservice/LibraryServiceApplication.java
@@ -1,8 +1,12 @@
 package com.bookcase.libraryservice;
 
+import com.bookcase.libraryservice.client.RetreiveMessageErrorDecoder;
+import feign.Logger;
+import feign.codec.ErrorDecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +16,13 @@ public class LibraryServiceApplication {
         SpringApplication.run(LibraryServiceApplication.class, args);
     }
 
+    @Bean
+    public ErrorDecoder errorDecoder(){
+        return new RetreiveMessageErrorDecoder();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
 }
