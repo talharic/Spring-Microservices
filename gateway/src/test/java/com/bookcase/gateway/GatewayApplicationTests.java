7f3d9685be1ce4db3e854d052663c5b7ae5b53f8
talharic@gmail.com <talharic@gmail.com>
2023-11-25 20:55:39 +0300

    add gateway

---
+++ gateway/src/test/java/com/bookcase/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookcase.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
